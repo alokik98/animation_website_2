44559458e4e962fe365715ab1e63120939438cfb
alokik98 <alorshsaxena12@gmail.com>
2023-07-10 23:17:37 +0530

    Completed landing page

--- script.js
+++ script.js
@@ -1,33 +1,36 @@
-gsap.to("#page1", {
-  scrollTrigger: {
-    trigger: "#page1",
-    start: `top top`,
-    end: `bottom top`,
-    markers: true,
-    pin: true,
-    scroller: "#main",
-  },
-});
-gsap.to("#page2", {
-  scrollTrigger: {
-    trigger: "#page2",
-    start: `top top`,
-    end: `bottom top`,
-    markers: true,
-    pin: true,
-    scroller: "#main",
-  },
-});
-gsap.to("#page3", {
-  scrollTrigger: {
-    trigger: "#page3",
-    start: `top top`,
-    end: `bottom top`,
-    markers: true,
-    pin: true,
-    scroller: "#main",
-  },
-});
+function locomotive() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true,
+  });
+  locoScroll.on("scroll", ScrollTrigger.update);
+
+  ScrollTrigger.scrollerProxy("#main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    },
+
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+
+    pinType: document.querySelector("#main").style.transform
+      ? "transform"
+      : "fixed",
+  });
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+  ScrollTrigger.refresh();
+}
+locomotive();
 
 const canvas = document.querySelector("canvas");
 const context = canvas.getContext("2d");
@@ -386,4 +389,55 @@ function scaleImage(img, ctx) {
   var hRatio = canvas.width / img.width;
   var vRatio = canvas.height / img.height;
   var ratio = Math.max(hRatio, vRatio);
+  var centerShift_x = (canvas.width - img.width * ratio) / 2;
+  var centerShift_y = (canvas.height - img.height * ratio) / 2;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(
+    img,
+    0,
+    0,
+    img.width,
+    img.height,
+    centerShift_x,
+    centerShift_y,
+    img.width * ratio,
+    img.height * ratio
+  );
 }
+
+ScrollTrigger.create({
+  trigger: "#page>canvas",
+  pin: true,
+  // markers: true,
+  scroller: "#main",
+  start: `top top`,
+  end: `600% top`,
+});
+
+gsap.to("#page1", {
+  scrollTrigger: {
+    trigger: `#page1`,
+    start: `top top`,
+    end: `bottom top`,
+    pin: true,
+    scroller: `#main`,
+  },
+});
+gsap.to("#page2", {
+  scrollTrigger: {
+    trigger: `#page2`,
+    start: `top top`,
+    end: `bottom top`,
+    pin: true,
+    scroller: `#main`,
+  },
+});
+gsap.to("#page3", {
+  scrollTrigger: {
+    trigger: `#page3`,
+    start: `top top`,
+    end: `bottom top`,
+    pin: true,
+    scroller: `#main`,
+  },
+});
